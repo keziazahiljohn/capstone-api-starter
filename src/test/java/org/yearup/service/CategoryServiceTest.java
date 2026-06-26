933f1d902acd1ac3d8b0d27a5c93d9a6ea1de0e4
keziazahiljohn <keziazahiljohn@gmail.com>
2026-06-25 23:51:31 -0400

    Mockito Testing - get all categories

--- src/test/java/org/yearup/service/CategoryServiceTest.java
+++ src/test/java/org/yearup/service/CategoryServiceTest.java
@@ -1,12 +1,21 @@
 package org.yearup.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.yearup.models.Category;
 import org.yearup.repository.CategoryRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+// methodName_StateUnderTest_ExpectedBehavior — for example byId_MissingId_ReturnsNull
 
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
@@ -16,5 +25,22 @@ class CategoryServiceTest {
     @InjectMocks
     private CategoryService categoryService;
 
+    @Test
+    void getAllCategories_CategoriesExist_ReturnAllCategories(){
+        // Arrange
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category(1, "Pink Vinyl", "vinyls"));
+        categories.add(new Category(2, "Blue Vinyl", "vinyl"));
+        categories.add(new Category(3, "Orange Vinyl", "vinyl"));
+        when(categoryRepository.findAll()).thenReturn(categories);
 
+        // Act
+        List<Category> result = categoryService.getAllCategories();
+
+        // Assist
+        assertNotNull(result);
+        assertEquals(3, result.size());
+
+        verify(categoryRepository).findAll();
+    }
 }
